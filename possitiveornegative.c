a0c20faf9cfbfa74992093dce96fe3bc8cc8a054
hajeeraai01 <hajeeraai01@gmail.com>
2024-01-04 13:42:25 +0530

    add

---
+++ possitiveornegative.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the value is a=");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("positive number",a);
+    }
+    else
+    {
+        printf("negative number",a);
+    }
+    return 0;
+}
